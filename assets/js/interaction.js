bd9e970418c919bb55022968c2ad2ee4ae24a1f9
camila badillo <camihyohana@MacBook-Air-de-camila.local>
2018-07-19 20:53:16 -0400

    ejercicio 6

--- assets/js/interaction.js
+++ assets/js/interaction.js
@@ -11,9 +11,16 @@ $(function () {
         event.stopPropagation();
         $(this).toggleClass('card__follow-btn--following');
     })
-    $('#image').change(function (event) {
+    $('#image').change(function (e) {
         var valor = $(event.target).val();
         var ruta = 'assets/images/squared/' + valor;
         $(this).parent().siblings('.create__image').children().attr('src', ruta);
+    })
+    $('#author').change(function (e) {
+        var foto = $(event.target).val();
+        var como = 'assets/images/profiles/' + foto;
+        $(this).parents('.create__display_images').children('.create__profile').children().attr('src', como);
+
+
     })
 });
