c39e2b7277973973462e9feed80f336f6e66a93c
camila badillo <camihyohana@MacBook-Air-de-camila.local>
2018-07-19 20:05:42 -0400

    ejeercicio 3 listo

--- assets/js/interaction.js
+++ assets/js/interaction.js
@@ -2,4 +2,9 @@ $(function () {
     $('.card').click(function () {
         $(this).toggleClass('card--open');
     })
+    $('.card__like').click(function (event) {
+        event.stopPropagation();
+        event.preventDefault();
+        $(this).toggleClass('card__like--red');
+    })
 });
